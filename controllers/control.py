2310fd6213f14c225d4808f65ac0d686f2583252
lucas gonzalez <luka.jesu12@gmail.com>
2013-09-18 18:29:07 -0300

    agrego funcion backup y restaurar

--- controllers/control.py
+++ controllers/control.py
@@ -3,3 +3,25 @@
 def inicio():
  
  return dict(message="Bienvenido")
+ 
+import os
+
+def backup():
+    # Copia de seguridad de la base de datos
+    
+    ruta = os.path.join (request.folder, "private" , "backup.csv") 
+    arch = open (ruta, "wb")
+    db.export_to_csv_file (arch)
+    arch.close()
+    
+    return 'Backup Realizado'
+    
+def restauracion():
+   
+   # Restaura la base de datos de seguridad
+    ruta = os.path.join (request.folder, "private" , "backup.csv") 
+    arch = open (ruta, "rb")
+   
+    db.import_from_csv_file(arch)
+   
+    return 'Restauración Completada'
